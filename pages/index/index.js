e6cb9b635033cc93d2d69ec49944399cf94ce41f
handsomeboyHT <>
2019-10-17 23:38:08 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,13 +9,22 @@ Page({
     taskList:[1,1,1,1,1,1,1,1,1,1,1,1,1],
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'), //判断是否可以使用
-    time: new Date().getFullYear()
+    time: new Date().getFullYear(),
+    listIndex: 0,
+    show: false
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
-    })
+    });
+  },
+  showList: function(e){
+    let v;
+    e.currentTarget.dataset.num === this.data.listIndex ? v = 0 : v = e.currentTarget.dataset.num;
+    this.setData({
+      listIndex: v
+    });
   },
   onLoad: function () {
     
