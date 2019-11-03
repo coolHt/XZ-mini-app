d6669b9b7aaedd10c271c3f4a2e93e37dd5c371a
handsomeboyHT <ht2631503150@gmail.com>
2019-11-04 00:27:35 +0800

    update

--- pages/resume/resume.js
+++ pages/resume/resume.js
@@ -13,7 +13,120 @@ Page({
     resumePhone: '',
     resumeBorth: '',
     resumeDiploma:'',
-    resumeWorks: ''
+    resumeWorks: '',
+    show: false,
+    selectTitle: '',
+    selectType:'',
+    borthArray: [ {
+      name: '1966'
+    }, {
+      name: '1967'
+    }, {
+      name: '1968'
+    }, {
+      name: '1969'
+    }, {
+      name: '1970'
+    }, {
+      name: '1971'
+    }, {
+      name: '1972'
+    }, {
+      name: '1973'
+    }, {
+      name: '1974'
+    }, {
+      name: '1975'
+    }, {
+      name: '1976'
+    }, {
+      name: '1977'
+    }, {
+      name: '1978'
+    }, {
+      name: '1979'
+    }, {
+      name: '1980'
+    }, {
+      name: '1981'
+    }, {
+      name: '1982'
+    }, {
+      name: '1983'
+    }, {
+      name: '1984'
+    }, {
+      name: '1985'
+    }, {
+      name: '1986'
+    }, {
+      name: '1987'
+    }, {
+      name: '1988'
+    }, {
+      name: '1989'
+    }, {
+      name: '1990'
+    }, {
+      name: '1991'
+    }, {
+      name: '1992'
+    }, {
+        name: '1993'
+      }, {
+        name: '1994'
+      }, {
+        name: '1995'
+      }, {
+        name: '1996'
+      }, {
+        name: '1997'
+      }, {
+        name: '1998'
+      }, {
+        name: '1999'
+      }, {
+        name: '2000'
+      }, {
+        name: '2001'
+      }, {
+        name: '2002'
+      }, {
+        name: '2003'
+      } ],
+    workTimeArray: [{
+        name: '无经验'
+      }, {
+        name: '应届生'
+      }, {
+        name: '1-3年'
+      }, {
+        name: '3-5年'
+      }, {
+        name: '5-10年'
+      }, {
+        name: '10年以上'
+      }, {
+        name: '高中以下'
+      }, {
+        name: '高中以下'
+      }],
+    diplomaArray: [{
+      name: '高中以下'
+    }, {
+      name: '高中'
+    }, {
+      name: '中专/技校'
+    }, {
+      name: '大专'
+    }, {
+      name: '本科'
+    }, {
+      name: '硕士'
+    }, {
+      name: '博士'
+    }],
+    selectAction:[]
   },
 
   /**
@@ -119,6 +232,52 @@ Page({
   },
   selectedArray: function(e){
     var sType = e.currentTarget.dataset.select;
+    var title = '';
+    var actionArray = [];
+    var selectType = '';
+    if (sType === 'borth') {
+      title = '请选择出生日期';
+      actionArray = this.data.borthArray;
+      selectType = 'borth';
+    } else if (sType === 'diploma') {
+      title = '请选择最高学历';
+      actionArray = this.data.diplomaArray;
+      selectType = 'diploma';
+    } else if (sType === 'workTime') {
+      title = '请选择工作时间';
+      actionArray = this.data.workTimeArray;
+      selectType = 'workTime';
+    }
+    this.setData({
+      show: true,
+      selectAction: actionArray,
+      selectTitle: title,
+      selectType: selectType
+      
+    });
     console.log(sType);
-  }
+  },
+  onClose() {
+      this.setData({
+        show: false
+      });
+    },
+
+    onSelect(event) {
+      //borth //diploma //workTime
+      var sType = this.data.selectType;
+      if (sType == 'borth'){
+        this.setData({
+          resumeBorth: event.detail.name
+        });
+      } else if (sType == 'diploma') {
+        this.setData({
+          resumeDiploma: event.detail.name
+        });
+      }else if(sType == 'workTime'){
+        this.setData({
+          resumeWorks: event.detail.name
+        });
+      }
+    }
 });
